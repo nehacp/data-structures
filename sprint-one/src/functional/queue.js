11b27f9f8b2a35b66a56d4bf61e60a68d6ed9c88
nehacp <neha.chaudhary@hotmail.com>
2017-08-23 15:24:04 -0700

    Stack complete, queue complete upto specific behavior"

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,22 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
+  var count = 0;
 
   someInstance.enqueue = function(value) {
+    count++;
+    storage[count] = value;
   };
 
   someInstance.dequeue = function() {
+    var last = storage[count];
+    delete storage[count];
+    count--;
+    return last;
   };
 
   someInstance.size = function() {
+    return (count < 0) ? 0 : count;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,23 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    count++;
+    storage[count] = value;
   };
 
   someInstance.pop = function() {
+    var lastElement = storage[count];
+    delete storage[count];
+    count--;
+    return lastElement;
   };
 
   someInstance.size = function() {
+    return (count < 0) ? 0 : count;
   };
 
   return someInstance;
