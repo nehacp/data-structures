c6dc311b6307bc8c886f888ae7f640f3fa4e889b
nehacp <neha.chaudhary@hotmail.com>
2017-08-24 14:19:03 -0700

    Complete tree functionality

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,19 +3,42 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var node = Tree(value);
+  this.children.push(node);
 };
 
 treeMethods.contains = function(target) {
-};
-
+  var bool = false;
+  function search(node) {
+  //check if self has value
+      bool = node.value === target;
+      //if yes, return true;
+      if (bool) {
+        return true;
+      } else {
+        //if not, check children length
+        if (node.children.length){
+          // if length, loop over children
+          //check if each child has value
+          bool = _.any(node.children, search);
+          //if child has value, return true
+          if (bool) {
+            return true;
+          }
+        }
+      }
+  };
+  search(this);
+  return bool;
+}
 
 
 /*
