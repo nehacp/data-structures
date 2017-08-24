25b580273c4316fbe477a33c9263cac909c11d61
nehacp <neha.chaudhary@hotmail.com>
2017-08-23 18:18:23 -0700

    addition of functional-prototypal Queue/Stack implementation

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(queueMethods);
+
+  obj.count = 0;
+  obj.remove = 0;
+  obj.storage = {};
+
+  return obj;
 };
 
-var queueMethods = {};
-
-
+var queueMethods = {
+  size: function() {
+    return this.count - this.remove;
+  },
+  enqueue: function(value) {
+    this.count++;
+    this.storage[this.count] = value;
+  },
+  dequeue: function() {
+    if(this.size() > 0) {
+      this.remove++;
+      var first = this.storage[this.remove];
+      delete this.storage[this.remove];
+      return first;
+    }
+  }
+};
