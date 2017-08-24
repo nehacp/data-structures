33481404b3ba93c116b5da00f04f91c127a990f4
nehacp <neha.chaudhary@hotmail.com>
2017-08-23 18:44:18 -0700

    addition of functional-pseudoclassical Queue/Stack implementation

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,25 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.remove = 0;
+  this.storage = {};
 };
 
+Queue.prototype.size = function() {
+    return this.count - this.remove;
+}
 
+Queue.prototype.enqueue = function(value) {
+    this.count++;
+    this.storage[this.count] = value;
+}
+
+Queue.prototype.dequeue = function() {
+    if(this.size() > 0) {
+      this.remove++;
+      var first = this.storage[this.remove];
+      delete this.storage[this.remove];
+      return first;
+    }
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,23 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.count = 0;
 };
 
+Stack.prototype.size = function() {
+    return (this.count < 0) ? 0 : this.count;
+  };
 
+
+Stack.prototype.push =  function(value) {
+    this.count++;
+    this.storage[this.count] = value;
+  }
+
+  Stack.prototype.pop = function (){
+    var last = this.storage[this.count];
+    delete this.storage[this.count];
+    this.count--;
+    return last;
+  }
