eaf385e1ba795e249d1b8c4d1ccc527e7b236ecd
nehacp <neha.chaudhary@hotmail.com>
2017-08-25 13:07:39 -0700

    addition of binarySearchTree insert function

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,9 +1,39 @@
 var BinarySearchTree = function(value) {
-	this.value = value;
-	this.left;
-	this.right;
+  this.value = value;
+  this.left = {};
+  this.right = {};
+};
+
+
+
+BinarySearchTree.prototype.insert = function(value) {
+  var tree = new BinarySearchTree(value);
+  this.traverse.call(this, tree);
+  //if less, check left property for null
+};
+
+BinarySearchTree.prototype.contains = function(value) {
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function() {
+
+};
+
+BinarySearchTree.prototype.traverse = function(newTree) {
+  if (Object.keys(this).length === 0) {
+    return newTree;
+  } else {
+    if (newTree.value < this.value) {
+      this.left = this.traverse.call(this.left, newTree);
+    } else {
+      this.right = this.traverse.call(this.right, newTree);
+    }
+  }
+  return this;
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
