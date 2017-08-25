596d72c58ee1e228d47c9e48bb6034eba3ec5e7e
nehacp <neha.chaudhary@hotmail.com>
2017-08-24 19:15:21 -0700

    Complete partial graph implementation

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -7,16 +7,19 @@ var Graph = function() {
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  //constant time
   this.storage[node] = {};
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  //constant time
   return this.storage[node] ? true : false;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  //linear time
   if (this.storage[node]) {
     for (var edge in this.storage[node]) {
       delete this.storage[edge][node];
@@ -27,23 +30,30 @@ Graph.prototype.removeNode = function(node) {
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  //constant time
  return Boolean(this.storage[fromNode][toNode]);
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  //constant time
   this.storage[fromNode][toNode] = true;
   this.storage[toNode][fromNode] = true;
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  //constant time
   delete this.storage[fromNode][toNode];
   delete this.storage[toNode][fromNode];
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  //linear time
+  _.each(this.storage, function(item, key) {
+    cb(key);
+  });
 };
 /*
  * Complexity: What is the time complexity of the above functions?
