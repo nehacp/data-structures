75673eccc34e0fd9f0353677ccb5d42f86c5042a
nehacp <neha.chaudhary@hotmail.com>
2017-08-26 18:10:00 -0700

    Partial implementation of BST rebalance

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -8,9 +8,17 @@ BinarySearchTree.prototype.insert = function(value) {
 	//log(n) time
   if (value < this.value) {
     (this.left) ? this.left = this.left.insert(value) : this.left = new BinarySearchTree(value);
-  } else {
+  } else if (value > this.value) {
     (this.right) ? this.right = this.right.insert(value) : this.right = new BinarySearchTree(value);
-  }
+  } 
+  
+  this._leftDepth = this.depth(this.left);
+  this._rightDepth = this.depth(this.right);
+  if (this._leftDepth / this._rightDepth > 2) {
+    this.rebalance('left');
+  } else if (this._rightDepth / this._leftDepth > 2) {
+    this.rebalance('right');
+  } 
   return this;
 };
 
@@ -41,6 +49,31 @@ BinarySearchTree.prototype.depthFirstLog = function(callback) {
 BinarySearchTree.prototype.depth = function(side) {
   //log(n) time
   return (side) ? Math.max(this.depth(side.left), this.depth(side.right)) + 1 : 0;
+};
+
+BinarySearchTree.prototype.rebalance = function(side) {
+  //
+  var temp;
+  var x = this.value;
+  // if side is left
+  if (side === 'left') {
+    // one-level down with its right
+    temp = this.left.right;
+  } else {
+    // else side is right
+    // one-level down with its left
+    temp = this.right.left;
+  }
+    this.value = temp.value;
+
+    this.insert(x);
+  // create temp binary tree with above node value
+ // var tree = new BinarySearchTree(temp.value);
+  // call cb on every other value in the exisiting tree
+  this.depthFirstLog(this.insert);
+
+  //  new theory
+  // collect all values by iterating and then populate all of them to new bst
 };
 
 // from exisiting head/root check its left and right tree
